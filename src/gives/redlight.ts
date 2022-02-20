3ec469cbdc7000cfc34f42f12cc77f23e94c3374
ian hitchcock <_@13h.io>
2022-02-19 19:37:08 -0800

    yeah probably should process the away team goals too

--- src/gives/redlight.ts
+++ src/gives/redlight.ts
@@ -95,6 +95,83 @@ export const getPlays = (bs: BoxScore, oldbs: BoxScore | null): PlayDTO[] => {
             });
         }
     }
+    // away
+    for (const [playerId, playerStats] of Object.entries(bs.away.players)) {
+        const compare = oldbs && oldbs.away.players[Number(playerId)];
+        let { goals, assists } = playerStats;
+        if (compare) {
+            goals = goals - compare.goals;
+            assists = assists - compare.assists;
+        }
+
+        // shoutcaster.error("GOALS: %O, ASSISTS: %O", goals, assists);
+        if (
+            compare &&
+            compare.outcome &&
+            compare.outcome !== playerStats.outcome
+        ) {
+            // if the previous recorded outcome exists (W or SO) but the new outcome is different, revert previous outcome
+            const id = v4();
+            const p = Number(playerId);
+            const t = compare.outcome;
+            const d = -1; // revert
+            const ts = bs.ts;
+            plays.push({
+                id,
+                p,
+                t,
+                d,
+                ts,
+            });
+        }
+        const compareOutcome = compare && compare.outcome;
+        if (playerStats.outcome && compareOutcome !== playerStats.outcome) {
+            // if the new outcome exists and is not already recorded, send outcome to mo
+            const id = v4();
+            const p = Number(playerId);
+            const t = playerStats.outcome;
+            const d = 1;
+            const ts = bs.ts;
+            plays.push({
+                id,
+                p,
+                t,
+                d,
+                ts,
+            });
+        }
+
+        // goals
+        for (let i = 0; i < Math.abs(goals); i++) {
+            const id = v4();
+            const p = Number(playerId);
+            const t = "G";
+            const d = (goals / Math.abs(goals)) as 1 | -1;
+            const ts = bs.ts;
+            plays.push({
+                id,
+                p,
+                t,
+                d,
+                ts,
+            });
+        }
+        // assists
+        for (let i = 0; i < Math.abs(assists); i++) {
+            const id = v4();
+            const p = Number(playerId);
+            const t = "A";
+            const d = (goals / Math.abs(assists)) as 1 | -1;
+            const ts = bs.ts;
+            plays.push({
+                id,
+                p,
+                t,
+                d,
+                ts,
+            });
+        }
+    }
 
     return plays;
 };
