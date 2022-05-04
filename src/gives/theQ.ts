3b383ed413227f275d245804e0a35b9b646175a9
ian hitchcock <_@13h.io>
2022-05-03 20:18:46 -0700

    Revert "add a max retry of 1 week to hookers"
    This reverts commit 9984cbb1b83b054ce06d038523a32f5d4a7be2f8.

--- src/gives/theQ.ts
+++ src/gives/theQ.ts
@@ -3,8 +3,6 @@ import { PlayDTO } from "../gos/plays";
 import { shoutcaster } from "../lockerroom/shoutcaster";
 import { hooking } from "./hooking";
 import { redlight } from "./redlight";
-
-const MAXTRY = 144 * 14; // 7 days of every 5 minutes.
 //Add jobs to the queue:
 
 export const hookingQ = new Queue("hookin");
@@ -17,30 +15,12 @@ const worker = new Worker<PlayDTO>("hookin", async (job) => {
             redlight(job.data);
             await hooking(job.data);
         } catch (e) {
-            shoutcaster.error("job failed, retrying in 5 min. \n%o", job.data);
-            // try again in 5 minutes // counting from 1 to keep things spicy
-            hookingQ.add("play-2", job.data, { delay: 5 * 60 * 1000 });
-        }
-    } else if (job.name.startsWith("play-")) {
-        // too lazy to change the job data to add retry count
-        const count = Number(job.name.split("-")[1]);
-        try {
-            shoutcaster.warn("retrying!!! attempt#: " + count);
-            redlight(job.data);
-            await hooking(job.data);
-        } catch (e) {
-            if (count >= MAXTRY) {
-                shoutcaster.error(
-                    "job failed for the last time QQ \n%o",
-                    job.data
-                );
-                return;
-            }
-            shoutcaster.error("job failed, retrying in 5 min. \n%o", job.data);
+            shoutcaster.error(
+                "job failed, retrying in 5 min. \n",
+                JSON.stringify
+            );
             // try again in 5 minutes
-            hookingQ.add(`play-${count + 1}`, job.data, {
-                delay: 5 * 60 * 1000,
-            });
+            hookingQ.add("play", job.data, { delay: 5 * 60 * 1000 });
         }
     }
 });
